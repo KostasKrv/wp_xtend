7e83a714c6d3bb5d65b8d84f82f891baa6642680
Konstantinos Krevatas <297mxq@gmail.com>
2019-07-15 17:43:21 +0300

    Translate ninja form fields with Polylang

--- NinjaFormsWrap.php
+++ NinjaFormsWrap.php
@@ -181,3 +181,87 @@ class NinjaFormsWrap
     }
 
 }
+
+
+/// Register form strings
+add_action('init', function()
+{
+    	if (!function_exists('pll_register_string')){
+		return;
+	}
+
+	$forms = array(2);
+
+	foreach ($forms as $form_id) {
+		$form_id = 2;
+		$GROUP_NAME = 'FORM_ID_' . $form_id;
+		$fields = Ninja_Forms()->form($form_id)->get_fields();
+
+		/*
+		echo '<pre>';
+		var_dump($fields);
+		echo '</pre>';
+		*/
+		
+		foreach ($fields as $field) {
+
+			$settings = $field->get_settings();
+			//echo '<pre>';var_dump($settings);echo '</pre>'; //debug
+
+			// Skip the hr
+			if ($settings['type'] == 'hr'){
+				continue;
+			}
+
+			/// Always add the label
+			pll_register_string($settings['label'], $settings['label'], $GROUP_NAME, true);
+
+			if (array_key_exists("default", $settings)) {
+				pll_register_string($settings['default'], $settings['default'], $GROUP_NAME, true);
+	    		}
+
+			if ($settings['type'] == 'submit'){
+				pll_register_string($settings['processing_label'], $settings['processing_label'], $GROUP_NAME, true);
+			}
+
+			if (!empty($settings['options'])) {
+	        	foreach($settings['options'] as $o){
+	        		pll_register_string($o['label'], $o['label'], $GROUP_NAME, true);
+	        	}
+	        }
+	}
+}
+
+},-1);
+
+/// Fetch the translated fields when showing the form
+function filter_ninja_forms_localize_fields( $field ) { 
+	if (!function_exists('pll__') || !is_array($field['settings']) ){
+		return $field;
+	}
+
+	
+    	$field['settings']['label'] = pll__($field['settings']['label']);
+
+
+    	if ($field['settings']['type'] === 'html' && array_key_exists("default", $field['settings'])) {
+        	$field['settings']['default'] = pll__($field['settings']['default']);
+    	}
+
+    	if ($settings['type'] == 'submit'){
+        	$field['settings']['processing_label'] = pll__($field['settings']['processing_label']);
+	}
+
+    	if (!empty($field['settings']['options'])) {
+        	foreach($field['settings']['options'] as $i => $o){
+        		$field['settings']['options'][$i]['label'] = pll__($o['label']);
+        	}
+    	}
+
+    	if ($field['settings']['label'] == 'Lang'){
+        	$field['settings']['default'] = pll_current_language('slug');
+	}  
+
+    	return $field; 
+}
+add_filter( 'ninja_forms_localize_fields', 'filter_ninja_forms_localize_fields', 10, 1 );
